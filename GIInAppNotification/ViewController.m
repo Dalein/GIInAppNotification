77025e6e6e5d5555475082c556edcbdf1c641a76
Gnatyuk Ivan <daleijn@mac-gnatyuk.local>
2016-06-14 11:00:15 +0600

    Initial Commit

---
+++ GIInAppNotification/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GIInAppNotification
+//
+//  Created by Gnatyuk Ivan on 14.06.16.
+//  Copyright © 2016 Gnatyuk Ivan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
